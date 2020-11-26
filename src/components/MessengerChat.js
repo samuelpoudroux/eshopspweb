5f82ff9c47582d28e6fa516dd57330bf1fd31888
samuelpoudroux <samuel.poudroux@alten.com>
2020-11-26 11:02:14 +0100

    add messenge

--- src/components/MessengerChat.js
+++ src/components/MessengerChat.js
@@ -4,5 +4,10 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 const { REACT_APP_FACEBOOK_PAGE_ID, REACT_APP_FACEBOOK_APP_ID } = process.env;
 
 export const MessengerChat = () => {
-  return <MessengerCustomerChat pageId="<PAGE_ID>" appId="<APP_ID>" />;
+  return (
+    <MessengerCustomerChat
+      pageId={REACT_APP_FACEBOOK_PAGE_ID}
+      appId={REACT_APP_FACEBOOK_APP_ID}
+    />
+  );
 };
