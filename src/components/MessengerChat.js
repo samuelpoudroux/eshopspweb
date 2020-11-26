71290dd977cfdd188a4dad6cbef67678780fddea
samuelpoudroux <samuel.poudroux@alten.com>
2020-11-26 13:23:52 +0100

    add messenge

--- src/components/MessengerChat.js
+++ src/components/MessengerChat.js
@@ -9,6 +9,7 @@ export const MessengerChat = () => {
       pageId={REACT_APP_FACEBOOK_PAGE_ID}
       appId={REACT_APP_FACEBOOK_APP_ID}
       language="FR"
+      themeColor="yellow"
     />
   );
 };
